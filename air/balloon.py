97a224eb9820885594b0d82c535170b959857463
Aaron Kay <kaychoro@gmail.com>
2022-02-08 16:46:42 -0700

    Update balloon.py
    Made it a little more encapsulated

--- air/balloon.py
+++ air/balloon.py
@@ -1,38 +1,38 @@
 class Balloon:
     def __init__(self, color):
-        self.popped = False
-        self.sealed = False
-        self.color = color
-        self.volume_ml = 0
-        self.max_volume_ml = 1000
-        self.update_time_ms = 0
+        self._popped = False
+        self._sealed = False
+        self._color = color
+        self._volume_ml = 0
+        self._max_volume_ml = 1000
+        self._update_time_ms = 0
 
     def pop(self):
-        self.popped = True
-        self.volume_ml = 0
+        self._popped = True
+        self._volume_ml = 0
 
     def fill(self, volume_ml):
-        self.volume_ml += volume_ml
-        if self.volume_ml > self.max_volume_ml:
+        self._volume_ml += volume_ml
+        if self._volume_ml > self._max_volume_ml:
             self.pop()
 
     def decrease_air(self, delta_ms):
         pass
 
     def __str__(self) -> str:
-        if self.popped:
+        if self._popped:
             return "<a big mess of elastic shreds>"
-        return self.color + ": " + str(self.volume_ml)
+        return self._color + ": " + str(self._volume_ml)
 
     def update(self, time_ms):
-        delta_ms = time_ms - self.update_time_ms
+        delta_ms = time_ms - self._update_time_ms
 
         #update the air if it's not sealed
-        if self.popped:
-            self.volume_ml = 0
-        elif not self.sealed and self.volume_ml > 0:
+        if self._popped:
+            self._volume_ml = 0
+        elif not self._sealed and self._volume_ml > 0:
             self.decrease_air(delta_ms)
-        elif self.volume_ml > self.max_volume_ml:
+        elif self._volume_ml > self._max_volume_ml:
             self.pop()
 
-        self.update_time_ms = time_ms
+        self._update_time_ms = time_ms
