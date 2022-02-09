07e54239efb045061250850e11b2573e0943c05b
Aaron Kay <kaychoro@gmail.com>
2022-02-09 09:20:24 -0700

    Update tank.py
    Made tank encapsulated!!

--- air/tank.py
+++ air/tank.py
@@ -1,20 +1,20 @@
 class Tank:
     def __init__(self, air_type):
-        self.volume_L = 0
-        self.max_volume_L = 25
-        self.air_type = air_type
+        self._volume_L = 0
+        self._max_volume_L = 25
+        self._air_type = air_type
     
     def release_air(self, volume_ml):
         amount = volume_ml / 1000
-        if amount <= self.volume_L:
-            self.volume_L -= amount
+        if amount <= self._volume_L:
+            self._volume_L -= amount
         else:
-            amount = self.volume_L
-            self.volume_L = 0
+            amount = self._volume_L
+            self._volume_L = 0
         return amount * 1000
     
     def fill(self, air_type = None):
         if air_type is not None:
-            self.air_type = air_type
-        self.volume_L = self.max_volume_L
+            self._air_type = air_type
+        self._volume_L = self._max_volume_L
             
